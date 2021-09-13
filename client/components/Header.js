554d84730d2caa14b41871574c6b79e7ecc34e4c
Hazel <hazel.na3@gmail.com>
2021-09-13 16:51:05 -0400

    react components

---
+++ client/components/Header.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const Header = () => {
+  return (
+    <header className='header'>
+      <div>this is header</div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ client/containers/ContentContainer.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+const ContentContainer = () => {
+  return (
+    <div className='content_container'>
+      <div>this is content</div>
+    </div>
+  );
+};
+
+export default ContentContainer;
