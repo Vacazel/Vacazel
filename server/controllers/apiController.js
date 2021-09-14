9904a958229f95cc6032ef93ab64d951d2378b90
Damien Evans <dnyceboy@gmail.com>
2021-09-14 16:22:58 -0400

    got all the data we need from the api in the backend requests

--- server/controllers/apiController.js
+++ server/controllers/apiController.js
@@ -1,10 +1,19 @@
 const dotenv = require('dotenv').config();
+const axios = require('axios');
 
 const apiController = {};
 
 apiController.getLatLong = async (req, res, next) => {
   try {
-    const result = await fetch(`https://api.opentripmap.com/0.1/en/places/geoname?name=seoul&apikey=${process.env.TRAVEL_API}`)
+    let result = {};
+    await axios.get(`https://api.opentripmap.com/0.1/en/places/geoname?name=seoul&apikey=${process.env.TRAVEL_API}`)
+    .then(response => {
+      const { name, lat, lon } = response.data;
+      result = { name, lat, lon};
+      res.locals.apiObj = result;
+    })
+    .catch(error => console.log(error))
+    next();
   } catch(err){
     console.log('error in getLatLong');
     next({
@@ -15,11 +24,53 @@ apiController.getLatLong = async (req, res, next) => {
 }
 
 apiController.getSuggestionsAndId = async (req, res, next) => {
-
+  const {name, lat, lon} = res.locals.apiObj;
+  try {
+    await axios.get(`https://api.opentripmap.com/0.1/en/places/autosuggest?name=${name}&radius=15000&lon=${lon}&lat=${lat}&apikey=${process.env.TRAVEL_API}`)
+    .then(response => {
+      console.log(response.data.features[0].properties.xid);
+      const featuresData = response.data.features
+      const result = [];
+      for(let i = 0; i < featuresData.length; i++){
+        result.push(featuresData[i].properties);
+        // console.log(featuresData[i].properties);
+      }
+      res.locals.locationData = result;
+    })
+    .catch(error => console.log(error))
+    next();
+  } catch(err){
+    console.log('error in getSuggestionsAndId');
+    next({
+      log: `apiController.getSuggestionsAndId: Error ${err}`,
+      message: {err: 'Error occurred in apiController.getSuggestionsAndId. Check server terminal'},
+    });
+  }
 }
 
 apiController.getImage = async (req, res, next) => {
-
+  
+  try {
+    const dataLength = res.locals.locationData.length
+    console.log(res.locals.locationData);
+    for(let i = 0; i < dataLength; i++){
+      const xid = res.locals.locationData[i]['xid']
+      await axios.get(`https://api.opentripmap.com/0.1/en/places/xid/${xid}?apikey=${process.env.TRAVEL_API}`)
+      .then(response => {
+      console.log(response.data.preview.source);
+      res.locals.locationData[i]['image'] = response.data.preview.source
+    })
+    .catch(error => console.log(error))
+    }
+    res.locals.locationData = res.locals.locationData.filter((obj, index) => 'image' in res.locals.locationData[index]);
+    next();
+  } catch(err){
+    console.log('error in getSuggestionsAndId');
+    next({
+      log: `apiController.getSuggestionsAndId: Error ${err}`,
+      message: {err: 'Error occurred in apiController.getSuggestionsAndId. Check server terminal'},
+    });
+  }
 }
 
 
--- server/routes/apiRouter.js
+++ server/routes/apiRouter.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const apiController = require('../controllers/apiController');
 
-router.get('/', (req, res) => {
-  res.status(200).json(res.locals.latLong);
+router.get('/', apiController.getLatLong, apiController.getSuggestionsAndId, apiController.getImage, (req, res) => {
+   res.status(200).json(res.locals.locationData);
 });
 
 module.exports = router;
