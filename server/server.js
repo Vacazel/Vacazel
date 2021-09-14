00f80c305b4b14b597c74fce320e3a50e94b285f
Damien Evans <dnyceboy@gmail.com>
2021-09-14 13:36:07 -0400

    created apirouter

--- server/server.js
+++ server/server.js
@@ -3,6 +3,11 @@ const path = require('path');
 const PORT = 3000;
 const app = express();
 const cors = require('cors');
+const apiRouter = require('./routes/apiRouter');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
   app.use('/build', express.static(path.join(__dirname, '../build')));
@@ -11,15 +16,29 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(express.static(path.resolve(__dirname, '../client')));
 
-app.use(express.static(path.resolve(__dirname, 'client')));
+app.get('/', (req, res) =>
+  res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'))
+);
 
-// app.get('/', (req, res) =>
-//   res.status(200).sendFile(path.resolve(__dirname, '../client/index.html'))
-// );
+app.use('/api', apiRouter);
+
+
+app.use('*', (req, res) => {
+  res.status(404).send('This url does not exist');
+})
+
+app.use((err, req, res, next) => {
+  const defaultError = {
+    log: 'Express error handler caught middleware error',
+    status: 500,
+    message: {error: 'An Error occured with server and middleware'},
+  };
+  const errorObj = Object.assign({}, defaultError, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+})
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
